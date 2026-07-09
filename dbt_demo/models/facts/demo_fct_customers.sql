68f86dc6e6d4bba0639cfa788521ded041392fd4
mohamedarshath-coder <mohamed.arshath@latentview.com>
2026-07-09 15:26:36 +0530

    demo reset: revert fct_customers to old refs + bare customer_segment

--- dbt_demo/models/facts/demo_fct_customers.sql
+++ dbt_demo/models/facts/demo_fct_customers.sql
@@ -9,7 +9,7 @@ final as (
         first_name,
         last_name,
         loyalty_segment                  as loyalty_score,
-        client_segment                 as segment_code,
+        customer_segment,
         is_active,
         acq_source,
         total_orders,
