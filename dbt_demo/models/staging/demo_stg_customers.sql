696058225a5e7089f40b54313e9d1f7a252d55cd
mohamedarshath-coder <mohamed.arshath@latentview.com>
2026-08-19 20:16:31 +0530

    Update demo_stg_customers.sql

--- dbt_demo/models/staging/demo_stg_customers.sql
+++ dbt_demo/models/staging/demo_stg_customers.sql
@@ -8,15 +8,15 @@ renamed as (
         last_name,
         email,
         phone,
-        customer_segment               as client_tier_segment, -- CHANGED
-        loyalty_tier                   as loyalty_score_v2,     -- CHANGED
+        customer_segment as customer_segment,
+        loyalty_tier     as loyalty_tier,
         registration_date,
         date_of_birth,
         city,
         state,
         country,
         marketing_opt_in,
-        acquisition_channel            as acq_channel
+        acquisition_channel as acq_channel
     from source
 )
 select * from renamed
